e636f875755253a6833e480befc7c932c87cce17
santhossunthar <santhoshsunthar@gmail.com>
2025-03-29 22:36:23 +0530

    Added user list API

--- users/urls.py
+++ users/urls.py
@@ -9,6 +9,7 @@ from users.views.email_verification_view import EmailVerificationView
 from users.views.passcode_view import PasscodeView
 from users.views.password_reset_request_view import PasswordResetRequestView
 from users.views.password_reset_view import PasswordResetView
+from users.views.users_list_view import UserListView
 
 urlpatterns = [
     path('registration', RegistrationView.as_view(), name='registration'),
@@ -18,4 +19,5 @@ urlpatterns = [
     path('otp', PasscodeView.as_view(), name='passcode'),
     path('password-reset-request', PasswordResetRequestView.as_view(), name='password_reset_request'),
     path('password-reset', PasswordResetView.as_view(), name='password_reset'),
+    path('list', UserListView.as_view(), name='user-list'),
 ]
